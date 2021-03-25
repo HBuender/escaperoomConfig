1cec1eabc277d1daadf0230abdc89ed9d8bc8931
Hendrik Bünder <hendrik.buender@helpline.pmcs.de>
2021-03-25 08:23:23 +0100

    Bash added

---
+++ restartEscapeRoom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Stop Container"
+docker stop escaperoom
+docker stop escaperoomclient
+
+echo "Remove Container"
+docker rm escaperoom
+docker rm escaperoomclient
+
+echo "Start Fresh Container"
+docker run -d -e "SPRING_PROFILES_ACTIVE=PD" -p8080:8080 --name escaperoom hbuender/escaperoom
+docker run -d -p80:80 --env API_URL="http://207.154.204.200:8080/"  --name escaperoomclient  hbuender/escaperoomclient
+docker run -d -p80:80 --env API_URL="http://localhost:8080/"  --name escaperoomclient  hbuender/escaperoomclient
