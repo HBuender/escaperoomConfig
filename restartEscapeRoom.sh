fa8bc484874a633c0a81b735f5f4e1a3104d33dc
Hendrik Bünder <hendrik.buender@helpline.pmcs.de>
2021-03-25 08:26:00 +0100

    Duplicate command removed

--- restartEscapeRoom.sh
+++ restartEscapeRoom.sh
@@ -11,4 +11,4 @@ docker rm escaperoomclient
 echo "Start Fresh Container"
 docker run -d -e "SPRING_PROFILES_ACTIVE=PD" -p8080:8080 --name escaperoom hbuender/escaperoom
 docker run -d -p80:80 --env API_URL="http://207.154.204.200:8080/"  --name escaperoomclient  hbuender/escaperoomclient
-docker run -d -p80:80 --env API_URL="http://localhost:8080/"  --name escaperoomclient  hbuender/escaperoomclient
+
